0177a455e1b4f6d1da11c1065cf0773ab184bcb8
BrendaRaquel4 <brendaraquel80@gmail.com>
2026-07-14 14:15:21 -0300

    refactor: fix regex performance and typevar mismatch safely

--- buku.py
+++ buku.py
@@ -75,7 +75,7 @@ PROMPTMSG = 'buku (? for help): '  # Prompt message string
 
 strip_delim = lambda s, delim=DELIM, sub=' ': str(s).replace(delim, sub)
 taglist = lambda ss: sorted(set(s.lower().strip() for s in ss if (s or '').strip()))
-parse_order = lambda order: [s for ss in order for s in re.split(r'\s*,\s*', ss.strip()) if s]
+parse_order = lambda order: [s.strip() for ss in order for s in ss.split(',') if s.strip()]
 like_escape = lambda s, c='`': s.replace(c, c+c).replace('_', c+'_').replace('%', c+'%')
 split_by_marker = lambda s: re.split(r'\s+(?=[.:>#*])', s)
 
